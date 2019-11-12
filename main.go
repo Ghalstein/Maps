b85a2c80714cf37d96371215023345f36f59764b
Garrett Halstein <garrett.halstein@emory.edu>
2019-11-12 12:31:58 -0500

    done

--- main.go
+++ main.go
@@ -8,6 +8,21 @@ func main() {
 		"red":   "#ff0000",
 		"black": "#000000",
 	}
-
 	fmt.Println(colors)
+
+	// square brace syntax
+	colors["white"] = "#ffffff"
+	fmt.Println(colors)
+
+	// deleting call where 1st arg is map and 2nd is key in the map
+	delete(colors, "red")
+	fmt.Println(colors)
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
