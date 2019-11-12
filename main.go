cc3f8faf435af775036dd9ee513da21c1b00593c
Garrett Halstein <garrett.halstein@emory.edu>
2019-11-12 12:18:45 -0500

    map example created

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declaring a string to string map
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"black": "#000000",
+	}
+
+	fmt.Println(colors)
+}
